0f74f3ba7d9a345a691b2a445330cf1ba47c46c3
BrocBlockchain <69422298+BrocBlockchain@users.noreply.github.com>
2021-04-02 11:14:43 +0800

    Update KaleidoERC721Mintable.sol

--- contracts/token/ERC721/KaleidoERC721Mintable.sol
+++ contracts/token/ERC721/KaleidoERC721Mintable.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.2;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract KaleidoERC721Mintable is ERC721, AccessControl {
+contract ERC721Mintable is ERC721, AccessControl {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) public {
