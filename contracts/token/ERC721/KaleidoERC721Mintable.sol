15f02e72ae2e5ef3373fc78c79305ab3431bb1c5
BrocBlockchain <69422298+BrocBlockchain@users.noreply.github.com>
2021-05-28 17:18:28 +0800

    Update KaleidoERC721Mintable.sol

--- contracts/token/ERC721/KaleidoERC721Mintable.sol
+++ contracts/token/ERC721/KaleidoERC721Mintable.sol
@@ -1,21 +1,44 @@
 pragma solidity ^0.6.2;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/AccessControl.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 
-contract ERC721Mintable is ERC721, AccessControl {
-    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+contract ERC721MintableBurnable is ERC721 {
+
+    uint256 private _currentTokenId = 0;
 
     constructor(string memory name, string memory symbol) ERC721(name, symbol) public {
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
     }
+    
+    function mintWithTokenURI(address to, string memory tokenURI) public {
+        uint256 newTokenId = _getNextTokenId();
+        _mint(_to, newTokenId);
+        _incrementTokenId();
+        _setTokenURI(newtokenId, tokenURI);
+        
+    }
+    
+        /**
+     * @dev calculates the next token ID based on value of _currentTokenId
+     * @return uint256 for the next token ID
+     */
+    function _getNextTokenId() private view returns (uint256) {
+        return _currentTokenId.add(1);
+    }
+    
+    
+   function _burn(uint256 tokenId) internal virtual {
+        address owner = ERC721.ownerOf(tokenId);
 
-    function mint(address to, uint256 tokenId) public {
-        _mint(to, tokenId);
+        _beforeTokenTransfer(owner, address(0), tokenId);
+
+        // Clear approvals
+        _approve(address(0), tokenId);
+
+        _balances[owner] -= 1;
+        delete _owners[tokenId];
+
+        emit Transfer(owner, address(0), tokenId);
     }
 
-    function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public {
-        mint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
-    }
 }
