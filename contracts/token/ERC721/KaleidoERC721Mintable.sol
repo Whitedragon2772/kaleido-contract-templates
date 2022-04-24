5216c8a6424afbbb607c08d1d5bf127031c0c014
Chief White Feather <78936234+Whitedragon2772@users.noreply.github.com>
2022-04-23 19:48:20 -0500

    update.sol

--- contracts/token/ERC721/KaleidoERC721Mintable.sol
+++ contracts/token/ERC721/KaleidoERC721Mintable.sol
@@ -18,6 +18,38 @@ contract KaleidoERC721Mintable is ERC721, AccessControl {
 
     function mintWithTokenURI(address to, uint256 tokenId, string memory tokenURI) public {
         mint(to, tokenId);
-        _setTokenURI(tokenId, tokenURI);
+        _setTokenURI(tokenId, tokenURI)
+    
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+/// @custom:security-contact whitefeather@whitebuffalotribe.onmicrosoft.com
+contract Draconian is ERC721, ERC721Enumerable, ERC721Burnable, Ownable {
+    constructor() ERC721("Draconian", "DGN") {}
+
+    function safeMint(address to, uint256 tokenId) public onlyOwner {
+        _safeMint(to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
